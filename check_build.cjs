8c9d060753eb6d35241f3be2db3e4fc8a1900379
maamarmordechaibp <maamarmordechaibp@gmail.com>
2026-09-03 17:03:53 -0400

    Cleanup temporary node scripts

--- check_build.cjs
+++
@@ -1,7 +0,0 @@
-﻿const { execSync } = require('child_process');
-try {
-  execSync('npm run build', { stdio: 'pipe', encoding: 'utf8' });
-  console.log("Success");
-} catch(e) {
-  console.log(e.stdout || e.stderr || e.message);
-}
