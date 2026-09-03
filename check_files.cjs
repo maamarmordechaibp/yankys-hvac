9029f3e17320669eb67f457084360ef77e79132c
maamarmordechaibp <maamarmordechaibp@gmail.com>
2026-09-03 17:15:44 -0400

    cleanup

--- check_files.cjs
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-const proposals = fs.readFileSync('src/pages/Proposals.jsx', 'utf8');
-const invoices = fs.readFileSync('src/pages/Invoices.jsx', 'utf8');
-
-// Just check for "+ Promo" to see where it lives
-console.log('Proposals has + Promo:', proposals.includes('+ Promo'));
-console.log('Invoices has + Promo:', invoices.includes('+ Promo'));
--- fix_sched2.cjs
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('src/pages/Schedule.jsx', 'utf8');
-code = code.replace("notes: \\Invoice for \\ job.\\", "notes: `Invoice for ${jobData.job_type} job.`");
-fs.writeFileSync('src/pages/Schedule.jsx', code);
-console.log('Fixed Schedule.jsx completely');
